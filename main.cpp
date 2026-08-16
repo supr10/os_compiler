3578e73d3d0b72be6f0a289bc7cd393252694994
bazin <bazinnolan2@gmail.com>
2026-08-16 15:50:25 +0200

    changed some files

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 static char logo[] = "the\n"
     "  ___  ____\n"
@@ -16,7 +17,8 @@ static char logo[] = "the\n"
     "| |_| |___) |\n"
     " \\___/|____/ compiler";
 
-
+static double version = 0.01;
+static char build_type[] = "alpha";
 
 static bool generate_compilation_file(char* filename) {                    //SO-Compiler uses nasm, later, it could use other things
     std::ofstream cfile("./compile.bat");
@@ -29,9 +31,9 @@ static bool generate_compilation_file(char* filename) {                    //SO-
 static int get_user_action(int argc, char** argv) {
     if (argc==1) {
         return 0;               // no argument selected
-    }if (argv[1]=="--version"||argv[1]=="-v") {
-        return 1;
-    }
+    }if (std::string(argv[1])=="version"||std::string(argv[1])=="-v") {
+        return 1;               //shows version and build
+    }return 2;                  //just compile the project
 }
 
 
@@ -39,8 +41,14 @@ int main(int argc, char** argv) {
     int choice = get_user_action(argc, argv);
     switch (choice) {
         case 0:
+            std::cout<<logo<<"\n"<<std::endl;
+            std::cerr<<"No action selected."<<std::endl;
+            return 1;
         case 1:
-            std::cout<<logo;
+            std::cout<<logo<<"\n"<<std::endl;
+            std::cout<<"The Os Compiler is currently on "<<build_type<<" and on build "<<version<<std::endl;
+            return 0;
+
     }
     return 0;
 }
